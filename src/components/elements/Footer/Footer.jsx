f230643970883bab54d08c3750e3610779c4df75
Павло Лопатюк <pavlolopatuk@Pashas-MacBook-Pro.local>
2023-04-22 11:59:35 +0300

    fixed link

--- src/components/elements/Footer/Footer.jsx
+++ src/components/elements/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <nav className={s.navigation}>
             <ul className={s.navigation__links}>
                 <li className={s.navigation__links_item} title='My telegram'>
-                    <a href="https://paullymantano.t.me/" target="_blank">
+                    <a href="https://t.me/pollymantano" target="_blank">
                         {SvgSelector('telegram')}
                     </a>
                 </li>
